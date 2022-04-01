b121bc80fcfbc157cff649834997deb87d301383
Nubiluma <71071950+Nubiluma@users.noreply.github.com>
2022-04-01 21:33:05 +0200

    edited text and its position (WIP) added method to capitalize text

--- src/main/java/de/cwansart/minecraft/seasoninfo/SeasonInfo.java
+++ src/main/java/de/cwansart/minecraft/seasoninfo/SeasonInfo.java
@@ -1,19 +1,24 @@
 package de.cwansart.minecraft.seasoninfo;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.ChatScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import sereneseasons.api.season.ISeasonState;
+import sereneseasons.api.season.Season;
 import sereneseasons.api.season.SeasonHelper;
 
 @Mod("seasoninfo")
 public class SeasonInfo {
+
+    private final MatrixStack matrixStack = new MatrixStack();
 
     public SeasonInfo() {
         MinecraftForge.EVENT_BUS.register(this);
@@ -23,16 +28,23 @@ public class SeasonInfo {
     public void onRender(TickEvent.RenderTickEvent event) {
         if (isInGame()) {
             ISeasonState clientSeasonState = SeasonHelper.dataProvider.getClientSeasonState();
-            String seasonName = clientSeasonState.getSeason().name();
-            String subSeasonName = clientSeasonState.getSubSeason().name();
+            String seasonName = capitalize(clientSeasonState.getSeason());
             int day = clientSeasonState.getDay();
-            int cycleDuration = clientSeasonState.getCycleDuration();
+            int daysLeftUntilNextSeason = (clientSeasonState.getSeasonDuration() / 24_000) - day;
+            int screenHeight = Minecraft.getInstance().getWindow().getHeight();
 
-            drawText(seasonName + ", " + subSeasonName, 1, 1);
-            drawText("Day: " + day, 1, 10);
-            drawText("Cycle duration: " + cycleDuration, 1, 20);
+            drawText(seasonName + ", Day: " + day, 1, (screenHeight - 20));
+            drawText(daysLeftUntilNextSeason + " days left until next season: ", 1, (screenHeight - 10));
         }
     }
+
+//    @SubscribeEvent
+//    public void onTickEvent(TickEvent.WorldTickEvent worldTickEvent){
+//        long worldDayTime = (worldTickEvent.world.getDayTime() / 24_000);
+//        drawText("Day time: " + worldDayTime, 1, 40);
+//        long worldGameTime = worldTickEvent.world.getGameTime();
+//        drawText("Game time: " + worldGameTime, 1, 50);
+//    }
 
     private boolean isInGame() {
         Screen screen = Minecraft.getInstance().screen;
@@ -42,6 +54,11 @@ public class SeasonInfo {
     private void drawText(String text, float x, float y) {
         int fallbackBlackColor = 0;
         int color = TextFormatting.WHITE.getColor() == null ? fallbackBlackColor : TextFormatting.WHITE.getColor();
-        Minecraft.getInstance().font.draw(new MatrixStack(), text, x, y, color);
+        Minecraft.getInstance().font.draw(matrixStack, text, x, y, color);
     }
+
+    private String capitalize(Season season) {
+        return season.name().charAt(0) + season.name().substring(1).toLowerCase();
+    }
+
 }
